ae90ab84c8729c5d07b7632efb21a7dac75776df
yoshidalucas <lucaslimayoshida@gmail.com>
2020-03-27 15:26:44 -0300

    exercicio2 triangulo1.js  adicionado

---
+++ exercises/4_2/exercise2/triangulo1.js
@@ -0,0 +1,7 @@
+let n = 5, myvar, array = [];
+//preencher array
+for (i=0; i<n; i++) {
+    array[i] = "*";             //atribui valor a array de indice i
+    console.log(array.join('')) //imprime array até onde já foi preenchida
+}
+
